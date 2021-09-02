c99900e65074c614db7ad908a47d24ee35ca1af1
Juan Pablo Arce <juanjprot@gmail.com>
2021-09-01 23:06:49 -0300

    Parallax fixes

--- Content/GFX/Shaders/Room.hlsl
+++ Content/GFX/Shaders/Room.hlsl
@@ -19,19 +19,20 @@ cbuffer cbFrag {
 
 struct VS_INPUT {
     float3 position : POSITION;
-    float3 normal : TEXCOORD0;
-    float3 tangent : TEXCOORD1;
-    float3 bitangent : TEXCOORD2;
-    float2 uv : TEXCOORD3;
+    float3 normal : NORMAL0;
+    float3 tangent : TANGENT0;
+    float3 bitangent : BITANGENT0;
+    float2 uv : TEXCOORD0;
 };
 
 struct PS_INPUT {
     float4 position : SV_POSITION;
-    float3 worldPos : TEXCOORD0;
-    float3 normal : TEXCOORD1;
-    float3 tangent : TEXCOORD2;
-    float3 bitangent : TEXCOORD3;
-    float2 uv : TEXCOORD4;
+    float3 worldPos : WORLDPOS0;
+    float3 normal : NORMAL0;
+    float3 tangent : TANGENT0;
+    float3 bitangent : BITANGENT0;
+    float2 uv : TEXCOORD0;
+
 };
 
 struct PS_OUTPUT {
@@ -56,12 +57,15 @@ PS_OUTPUT PS(PS_INPUT input) {
 
     float3 viewDir = normalize(viewPos - input.worldPos);
 
-    float3x3 worldToTangent = float3x3(input.tangent, input.bitangent, input.normal);
+    float3 t = -input.tangent;
+    float3 b = -input.bitangent;
+    float3 n = -input.normal;
+    float3x3 worldToTangent = float3x3(t, b, n);
     float3 transDir = mul(worldToTangent, viewDir);
-    float2 uv = input.uv + transDir.xy / transDir.z * disp.Sample(smp, input.uv).r * 0.1;
+    float2 uv = input.uv - transDir.xy / transDir.z * (1.0 - disp.Sample(smp, input.uv).r) * 0.03;
 
-    float3 normSmp = norm.Sample(smp, input.uv).rgb;
-    float3 normal = (normSmp.r - 0.5) * 2.0 * input.tangent + (normSmp.g - 0.5) * 2.0 * input.bitangent + normSmp.b * input.normal;
+    float3 normSmp = norm.Sample(smp, uv).rgb;
+    float3 normal = normalize((normSmp.r - 0.5) * 2.0 * input.tangent + (normSmp.g - 0.5) * 2.0 * input.bitangent + normSmp.b * input.normal);
 
     float3 dist = lightPos - input.worldPos;
     float acDist = 1.0 - saturate((dist.x * dist.x + dist.y * dist.y + dist.z * dist.z) / effectiveRangeSquared);
@@ -70,13 +74,9 @@ PS_OUTPUT PS(PS_INPUT input) {
     float diffuse = saturate(dot(lightDir, normal));
     
     float3 reflectDir = normalize(2.0 * diffuse * normal - lightDir);
-    float roughness = rough.Sample(smp, input.uv).r;
+    float roughness = rough.Sample(smp, uv).r;
     float specular = pow(saturate(dot(viewDir, reflectDir)), 64);
-    
-    if (roughness != 1.987) {
-        output.color = float4(((diff.Sample(smp, input.uv).rgb * diffuse) + (1.0 - roughness) * saturate(4 * diffuse) * specular) * acDist * intensity, 1.0);
-    } else {
-        output.color = diff.Sample(smp, uv);
-    }
+
+    output.color = float4(((diff.Sample(smp, uv).rgb * diffuse) + (1.0 - roughness) * saturate(4 * diffuse) * specular) * acDist * intensity, 1.0);
     return output;
 }
