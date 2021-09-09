3f8f13053b605a431d9c39255b0e626cc7192a61
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-09-10 00:11:48 +0200

    Toggled accidentally left on flag

--- Tools/Bverter/BVerter.cpp
+++ Tools/Bverter/BVerter.cpp
@@ -116,7 +116,7 @@ int main(int argc, char** argv) {
     }
 
     bool recompile = false;
-#if 1 // Set to 1 when working on this.
+#if 0 // Set to 1 when working on this.
     recompile = true;
 #endif
 
